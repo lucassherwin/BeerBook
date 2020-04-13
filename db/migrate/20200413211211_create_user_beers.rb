0eebdf755629d6fd63d0d84146be477f27e13a0f
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-13 17:31:08 -0400

    add attributes to user_beer table migration file

--- db/migrate/20200413211211_create_user_beers.rb
+++ db/migrate/20200413211211_create_user_beers.rb
@@ -1,6 +1,8 @@
 class CreateUserBeers < ActiveRecord::Migration[6.0]
   def change
     create_table :user_beers do |t|
+      t.integer :user_id 
+      t.integer :beer_id
 
       t.timestamps
     end

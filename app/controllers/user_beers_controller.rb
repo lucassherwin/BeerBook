42ea6a96e5c4965992270229cf67bd942709e571
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-13 17:29:38 -0400

    add user_beer controller logic, new view, routes

--- app/controllers/user_beers_controller.rb
+++ app/controllers/user_beers_controller.rb
@@ -1,3 +1,29 @@
 class UserBeersController < ApplicationController
-  
+  def new
+    @user_beer = UserBeer.new
+    @beers = Beer.all
+  end
+
+  def create 
+    @user_beer = UserBeer.create(user_id: session[:user_id], user_beer_params)
+
+    if @user_beer.valid?
+      redirect_to @user_beer.user 
+    else
+      render :new 
+    end
+  end
+
+  def destroy
+    @user_beer = UserBeer.find(params[:id])
+    user = @user_beer.user
+    @user_beer.destroy
+    redirect_to user_path(user.id)
+  end
+
+  private 
+
+  def user_beer_params
+    params.require(:user_beer).permit(:beer_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_beers
+  resources :user_beers, only: [:new, :create, :destroy]
   resources :beers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new'

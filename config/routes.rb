f4ce35b22a3e736dcb8362554803597226af1884
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-14 13:23:29 -0400

    create sessions controller and new view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new'
   resources :users, except: [:index, :new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end

2fcd2f49be81e33c953c7265993c48aec8f27fa6
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-14 10:15:41 -0400

    add user_brewery controller actions and logic

--- app/controllers/user_breweries_controller.rb
+++ app/controllers/user_breweries_controller.rb
@@ -1,2 +1,23 @@
 class UserBreweriesController < ApplicationController
+
+  def new 
+    @user_brewery = UserBrewery.new
+  end
+
+  def create 
+    @user_brewery = UserBrewery.create(user_brewery_params)
+
+    if @user_brewery.valid?
+      redirect_to @user_brewery.user
+    else
+      render :new 
+    end
+  end
+
+  def destroy
+    @user_brewery = UserBrewery.find(params[:id])
+    user = @user_brewery.user
+    @user_brewery.destroy
+    redirect_to user
+  end
 end

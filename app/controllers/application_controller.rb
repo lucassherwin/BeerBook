853b4df731b3d9282b3b5a3ae0c2fbe0ff64072e
lucassherwin <lksherwin@gmail.com>
2020-04-15 13:35:47 -0400

    mroe stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :authorize
 
   def current_user
     @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def authorize
+    unless @current_user
+      redirect_to login_path
+    end
   end
 end

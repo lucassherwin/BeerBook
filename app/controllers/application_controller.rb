4c5d87d2b5622a7152a520b90c32d3621ebde8ca
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-16 10:05:28 -0400

    update application controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,15 @@
 class ApplicationController < ActionController::Base
   # before_action :current_user
   # skip_before_action :authorize, only: [:new, :create]
-  helper_method :current_user, :logged_in?
+  helper_method :current_user
+  before_action :current_user, :authorize
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  def logged_in?
-    !!current_user
-  end
-
-  def require_user
-    unless logged_in?
+  def authorize
+    unless current_user
       # flash[:danger] = "You must be logged in to perform that action"
       redirect_to root_path
     end

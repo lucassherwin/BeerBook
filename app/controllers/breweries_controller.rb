ee8f992e4bc94d21b1e3b21ad7cc96cffbec16de
lucassherwin <lksherwin@gmail.com>
2020-04-16 13:13:17 -0400

    fixing breweries show when no website

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -14,14 +14,15 @@ class BreweriesController < ApplicationController
     unparsed_brew_data = RestClient.get("https://data.opendatasoft.com/api/records/1.0/search/?dataset=open-beer-database%40public-us&facet=style_name&facet=cat_name&facet=name_breweries&facet=country&refine.name_breweries=#{name}")
     parsed_brew_data = JSON.parse(unparsed_brew_data)
     
-    @website = parsed_brew_data['records'][0]['fields']['website']
+    if parsed_brew_data['records'][0]['fields']['website'] == nil
+      @website = "No Website Listed"
+    else
+      @website = parsed_brew_data['records'][0]['fields']['website']
+    end
 
-    # @beer_name = []
-    # @beer_desc = []
     @beer_hash = {}
     parsed_brew_data['records'].each do |b|
       @beer_hash[b['fields']['name']] =  b['fields']['descript']
     end
-    # byebug
   end
 end

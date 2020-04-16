31970b9bbb9e83811ed32c197926bfec19a965ce
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-16 11:06:31 -0400

    include semantic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// semantic ui
+require("semantic-ui-sass")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
